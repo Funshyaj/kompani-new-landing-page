51c25693e9f730df8822a6ee881d1830ebce3f67
Funshyaj <Funshoajayi29@gmail.com>
2023-11-03 23:49:45 +0100

    project set up and few sections done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kompani new landing page
- 
+
+## To run locally
+
+`` bash
+npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
+
+``
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        warning: "#B54708",
+        faded: "#F6F6F6",
+      },
+    },
+  },
+  plugins: [],
+};
